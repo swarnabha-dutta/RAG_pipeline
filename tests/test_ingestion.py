fe1fc9a00703bb11e48ab859a38a3c5916c10a6a
swarnabha <swarnabhadutta909@gmail.com>
2026-08-07 22:24:44 +0530

    add some supporting pdfs

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -3,7 +3,7 @@ from app.services.ingestion_service import IngestionService
 service = IngestionService()
 
 result = service.ingest_document(
-    "data/pdfs/AI Python Engineering Assignment.pdf"
+    "data/pdfs/02_Maneka_Gandhi_vs_Union_of_India_p0683-p0794.pdf"
 )
 
 print(result)
